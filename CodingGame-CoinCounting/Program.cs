e33221b2d4d89b75dc032c346182ff28dff36ae0
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-02-22 19:30:44 +0100

    Basic main
    Getting input, operating logic in given testable function from input and printing it out for coding game

---
+++ CodingGame-CoinCounting/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CoinCounting
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var valueToReach = int.Parse(Console.ReadLine());
+            var N = int.Parse(Console.ReadLine());
+            var nbOfCoins = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            var values = new int[N];
+            var splitted = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                values[i] = int.Parse(splitted[i]);
+            }
+            Console.WriteLine(Process(valueToReach, N, nbOfCoins, values));
+        }
+        public static int Process(int valueToReach, int N, int[] nbOfCoins, int[] values)
+        {
+            return (valueToReach / values[0]) + 1;
+        }
+    }
+}
