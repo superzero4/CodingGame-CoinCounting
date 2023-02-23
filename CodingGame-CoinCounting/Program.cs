eeaf9735e146adfdd74979060aff500dea7cb13d
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-02-23 20:27:24 +0100

    Added both command line and console In support
    For dev purpose, running the debugger with specific test cases, in coding game IDE we'll only keep Console.In without the abstraction

--- CodingGame-CoinCounting/Program.cs
+++ CodingGame-CoinCounting/Program.cs
@@ -1,20 +1,41 @@
 ﻿namespace CoinCounting
 {
-
+    public interface IConsole
+    {
+        string ReadLine();
+    }
+    public class ConsoleFacade : IConsole
+    {
+        public string ReadLine() => Console.ReadLine();
+    }
+    public class StreamConsole : IConsole
+    {
+        TextReader _sr;
+        public string ReadLine()
+        {
+            return _sr.ReadLine();
+        }
+        public StreamConsole(string args)
+        {
+            _sr = new StringReader(args);
+        }
+    }
     public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            var valueToReach = int.Parse(Console.ReadLine());
-            var N = int.Parse(Console.ReadLine());
-            var nbOfCoins = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            IConsole entry = new StreamConsole(string.Join('\n', args).Replace('.', ' '));
+            //IConsole Console= new ConsoleFacade();
+            var valueToReach = int.Parse(entry.ReadLine());
+            var N = int.Parse(entry.ReadLine());
+            var nbOfCoins = entry.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
             var values = new int[N];
-            var splitted = Console.ReadLine().Split(' ');
+            var splitted = entry.ReadLine().Split(' ');
             for (int i = 0; i < N; i++)
             {
                 values[i] = int.Parse(splitted[i]);
             }
-            Console.WriteLine(Process(valueToReach, N, nbOfCoins, values));
+            System.Console.WriteLine(Process(valueToReach, N, nbOfCoins, values));
         }
         public static int Process(int valueToReach, int N, int[] nbOfCoins, int[] values)
         {
