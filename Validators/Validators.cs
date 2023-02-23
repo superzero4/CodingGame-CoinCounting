5c1c525e51596e46584416f5a0131525ae68a6c6
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-02-23 20:24:32 +0100

    Added another coherent validator

--- Validators/Validators.cs
+++ Validators/Validators.cs
@@ -15,6 +15,7 @@ namespace Validators
 
         [TestCase(16, 14, 4, new int[] { 2, 3, 4, 5 }, new int[] { 4, 5, 2, 8 })]
         [TestCase(9, 14, 3, new int[] { 4, 3, 4, 5 }, new int[] { 4, 5, 2, 8 })]
+        [TestCase(3, 14, 2, new int[] { 2,2 }, new int[] { 6,5 })]
         [TestCase(3, 9, 1, new int[] { 3 }, new int[] { 4 })]
         public void Test(int result, int valueToReach, int N, int[] nbOfCoins, int[] values)
         {
