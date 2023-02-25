598ddbe82f9d305821fa344650a364820346cc70
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-02-25 08:52:37 +0100

    Updated referencs using correct subclasses in tests

--- Validators/Validators.cs
+++ Validators/Validators.cs
@@ -11,10 +11,10 @@ namespace Validators
         public void Setup()
         {
 
-        }       
-        public void Test<T>(int result, int valueToReach, int N, int[] nbOfCoins, int[] values) where T : Program.IProcesser, new()
+        }
+        public void Test<T>(int result, int valueToReach, int N, int[] nbOfCoins, int[] values) where T : Program.Processers.IProcesser, new()
         {
-            var obtained = new T().Process(valueToReach,N, nbOfCoins, values);
+            var obtained = new T().Process(valueToReach, N, nbOfCoins, values);
             //Assert.AreEqual(consoleOut.ToString(), "Hello, World!");
             Assert.AreEqual(result, obtained);
         }
@@ -22,11 +22,11 @@ namespace Validators
         [TestCase(9, 14, 3, new int[] { 4, 3, 4, 5 }, new int[] { 4, 5, 2, 8 })]
         [TestCase(3, 14, 2, new int[] { 2, 2 }, new int[] { 6, 5 })]
         [TestCase(3, 9, 1, new int[] { 3 }, new int[] { 4 })]
-        public void TestStack(int result, int valueToReach, int N, int[] nbOfCoins, int[] values)=> Test<Program.StackProcesser>(result, valueToReach, N, nbOfCoins, values);
+        public void TestStack(int result, int valueToReach, int N, int[] nbOfCoins, int[] values) => Test<Program.Processers.StackProcesser>(result, valueToReach, N, nbOfCoins, values);
         [TestCase(16, 14, 4, new int[] { 2, 3, 4, 5 }, new int[] { 4, 5, 2, 8 })]
         [TestCase(9, 14, 3, new int[] { 4, 3, 4, 5 }, new int[] { 4, 5, 2, 8 })]
         [TestCase(3, 14, 2, new int[] { 2, 2 }, new int[] { 6, 5 })]
         [TestCase(3, 9, 1, new int[] { 3 }, new int[] { 4 })]
-        public void TestArray(int result, int valueToReach, int N, int[] nbOfCoins, int[] values)=> Test<Program.ArrayProcessor>(result, valueToReach, N, nbOfCoins, values);
+        public void TestArray(int result, int valueToReach, int N, int[] nbOfCoins, int[] values) => Test<Program.Processers.ArrayProcessor>(result, valueToReach, N, nbOfCoins, values);
     }
 }
